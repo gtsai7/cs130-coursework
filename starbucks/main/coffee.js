35ee3b185a15cd0daa8ed46fc4c5df60e13f87b4
gtsai7 <garytsai2022@u.northwestern.edu>
2020-06-03 21:58:10 -0700

    filter flex

--- starbucks/main/coffee.js
+++ starbucks/main/coffee.js
@@ -133,9 +133,9 @@ filter = (ev) => {
   let checked = ev.target.checked;
   console.log(ev.target.value);
   console.log(document.querySelectorAll(ev.target.value));
-  if (ev.target.value === ".Iced.Coffees") {
+  if (ev.target.value.includes("Clover®") && !ev.target.value.includes("Iced")) {
     console.log(ev.target.value);
-     if (checked) {
+    if (checked) {
       document.querySelectorAll(ev.target.value)[2].style.display = 'flex';
       document.querySelectorAll(ev.target.value)[3].style.display = 'flex';
     }
@@ -144,10 +144,9 @@ filter = (ev) => {
       document.querySelectorAll(ev.target.value)[3].style.display = 'none';
     }
   }
-  else
-  if (ev.target.value.includes("Clover®") && !ev.target.value.includes("Iced")) {
+  if (ev.target.value === ".Iced.Coffees") {
     console.log(ev.target.value);
-    if (checked) {
+     if (checked) {
       document.querySelectorAll(ev.target.value)[2].style.display = 'flex';
       document.querySelectorAll(ev.target.value)[3].style.display = 'flex';
     }
